f7307f52c18b0a9801b2aae272b6780ace939432
Poleto <diego.souza@bling.com.br>
2021-01-13 08:48:34 -0300

    index split to foot and header

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '36d092ad30a337b94f24db79620a1436e96079b8',
+    'name' => 'dev/treinamento',
+  ),
+  'versions' => 
+  array (
+    'dev/treinamento' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '36d092ad30a337b94f24db79620a1436e96079b8',
+    ),
+  ),
+);
